a3fcf49e0cbf0dad917bd5b7d35a6689fe8203a0
Adit0507 <adi05072002@gmail.com>
2024-08-06 22:30:24 +0530

    adding methods & testcases

--- pocketLog/logger.go
+++ pocketLog/logger.go
@@ -1,17 +1,47 @@
 package pocketlog
 
+import (
+	"fmt"
+	"io"
+	// "os"
+)
+
 type Logger struct {
 	threshold Level
+	output    io.Writer
 }
 
-func New(threshold Level) *Logger{
+func New(threshold Level, output io.Writer) *Logger {
 	return &Logger{
 		threshold: threshold,
+		output:    output,
 	}
 }
 
-func (l*Logger) Debugf(format string, args ...any){}
+func (l *Logger) Debugf(format string, args ...any) {
+	if l.threshold > LevelDebug {
+		return
+	}
 
-func(l*Logger) Infof(format string, args ...any){}
+	l.logf(format, args...)
+}
 
-func(l *Logger) Errorf(format string, args ...any){}
+func (l *Logger) logf(format string, args ...any) {
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+}
+
+func (l *Logger) Infof(format string, args ...any) {
+	if l.threshold > LevelInfo {
+		return
+	}
+
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+}
+
+func (l *Logger) Errorf(format string, args ...any) {
+	if l.threshold > LevelError {
+		return
+	}
+
+	_, _ = fmt.Fprintf(l.output, format+"\n", args...)
+}
--- pocketLog/logger_test.go
+++ pocketLog/logger_test.go
@@ -2,9 +2,10 @@ package pocketlog_test
 
 import (
 	pocketlog "logging/pocketLog"
+	"os"
 )
 
 func ExampleLogger_Debugf() {
-	debugLogger := pocketlog.New(pocketlog.LevelDebug)
+	debugLogger := pocketlog.New(pocketlog.LevelDebug, os.Stdout)
 	debugLogger.Debugf("Hello, %s", "world")
 }
