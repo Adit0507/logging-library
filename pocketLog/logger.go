b9441af2127a91f8cebd04cf261b4a2212cee63b
Adit0507 <adi05072002@gmail.com>
2024-08-06 12:00:10 +0530

    test cases

--- pocketLog/logger.go
+++ pocketLog/logger.go
@@ -1,6 +1,17 @@
 package pocketlog
 
 type Logger struct {
-	
+	threshold Level
 }
 
+func New(threshold Level) *Logger{
+	return &Logger{
+		threshold: threshold,
+	}
+}
+
+func (l*Logger) Debugf(format string, args ...any){}
+
+func(l*Logger) Infof(format string, args ...any){}
+
+func(l *Logger) Errorf(format string, args ...any){}
